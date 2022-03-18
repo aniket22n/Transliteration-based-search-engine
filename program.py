8466262a61f9522620c5a1d2a5daee7409ff04d9
Aniket <ap8888678005@gmail.com>
2022-03-18 20:11:17 +0530

    Tesseract ocr added

--- program.py
+++ program.py
@@ -1,3 +1,4 @@
+from distutils.command.config import LANG_EXT
 from flask import Flask, jsonify, render_template, redirect, url_for, request, flash, json
 # from flask_mongoengine import MongoEngine
 from flask_pymongo import PyMongo
@@ -6,6 +7,11 @@ from werkzeug.utils import secure_filename
 import os
 import PyPDF2
 from PyPDF2 import PdfFileReader
+import PIL
+from PIL import Image
+import pytesseract as pt
+# import easyocr
+# import cv2 as cv
 #import urllib.request
 
 program = Flask(__name__)  #program is a variable that contains a website and load in memory 
@@ -49,30 +55,42 @@ def upload():
     filename = secure_filename(file.filename) #secure_filename to make sure data can't be forged
    
     if file and allowed_file(file.filename): #file shouldn't empty and shoudn't be any other extension 
-       file.save(os.path.join(program.config['UPLOAD_FOLDER'], filename))  #save() method to save file on location 
-       #    usersave = User(file_name=file.filename) #calling user class
-       #    usersave.save() #to save file name in db
-       if '.' in filename and filename.rsplit('.', 1)[1].lower() == "pdf": #checking whether the  file is pdf or not  
-           f = open("C:\\Users\\ap888\\Desktop\\Internship CLIDE\\Transliteration_flask\\static\\saved_files\\" + file.filename,'rb') #pdf files with images are not allowed
-           Pdfreader = PyPDF2.PdfFileReader(f) #pdf reader object 
-           text = ""
-           for i in range(22,25):
-               page = Pdfreader.getPage(i)
-               text = text + page.extractText()
-           db.user.insert_one({'file_name': file.filename, 'content' : text}) 
-           f.close()
-       else:
-            f = open("C:\\Users\\ap888\\Desktop\\Internship CLIDE\\Transliteration_flask\\static\\saved_files\\" + file.filename,'r',encoding = 'utf-8') #text file
-            text = f.read()   #read the entire content, it tet should be UTF-8 text
-            db.user.insert_one({'file_name': file.filename, 'content' : text}) #insert content into database
+        file.save(os.path.join(program.config['UPLOAD_FOLDER'], filename))  #save() method to save file on location 
+        #    usersave = User(file_name=file.filename) #calling user class
+        #    usersave.save() #to save file name in db
+        if '.' in filename and filename.rsplit('.', 1)[1].lower() == "pdf": #checking whether the  file is pdf or not  
+            f = open("C:\\Users\\ap888\\Desktop\\Internship CLIDE\\Transliteration_flask\\static\\saved_files\\" + file.filename,'rb') #pdf files with images are not allowed
+            Pdfreader = PyPDF2.PdfFileReader(f) #pdf reader object 
+            text = ""
+            for i in range(22,25):
+                page = Pdfreader.getPage(i)
+                text = text + page.extractText()
+            db.user.insert_one({'file_name': file.filename, 'content' : text}) 
             f.close()
-    #    db.user.insert(text_file_doc)
-       flash('File successfully uploaded ' + file.filename + ' to the database!') # flash message 
-    #    file_names = db.user.find()
-       return redirect('/') # redirect of main page
+        elif '.' in filename and filename.rsplit('.', 1)[1].lower() == "txt":
+                f = open("C:\\Users\\ap888\\Desktop\\Internship CLIDE\\Transliteration_flask\\static\\saved_files\\" + file.filename,'r',encoding = 'utf-8') #if the file is text file
+                text = f.read()   #read the entire content, it tet should be UTF-8 text
+                db.user.insert_one({'file_name': file.filename, 'content' : text}) #insert content into database
+                f.close()
+        else: # if file is image file
+            pt.pytesseract.tesseract_cmd = r'C:\Users\ap888\Desktop\Internship CLIDE\Transliteration_flask\env\Tesseract.exe' #specifying the path of tesseract / it can also be dont using environment variable 
+            img = Image.open("C:\\Users\\ap888\\Desktop\\Internship CLIDE\\Transliteration_flask\\static\\saved_files\\" + file.filename)
+            text = pt.image_to_string(img, lang="hin") #only working for hindi language
+            db.user.insert_one({'file_name': file.filename, 'content' : text})
+
+        #    db.user.insert(text_file_doc)
+        flash('File successfully uploaded ' + file.filename + ' to the database!') # flash message 
+        #    file_names = db.user.find()
+        return redirect('/') # redirect of main page
     else:
        flash('Invalid Uplaod only txt, pdf, png, jpg, jpeg, gif') #flash message,if condition not satisfied 
     return redirect('/')    
+
+
+
+
+
+
 
 
 # This fucntion is used to get input value from user
